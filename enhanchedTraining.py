cd09f3eb5782d20c7d0870eb6064b193fad2c936
brataccas99 <g.lodato15@studenti.unisa.it>
2024-01-01 18:41:34 +0100

    looks pretty good from the 16th epoch

--- enhanchedTraining.py
+++ enhanchedTraining.py
@@ -8,168 +8,161 @@ from torch.optim import lr_scheduler
 import tqdm
 from sklearn.metrics import classification_report, confusion_matrix
 
-def main():
-    # Enable CUDNN benchmarks
-    torch.backends.cudnn.benchmark = True
+def load_data(input_path, batch_size):
+    data_transforms = transforms.Compose([
+        transforms.Resize((512, 512)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
 
-    # Data normalization
-    data_transforms = transforms.Compose(
-        [
-            transforms.Resize((512, 512)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-
-    # Load the dataset
-    input_path = "./trainingDataset/Hair Diseases - Final"
-    BATCH_SIZE = 8
     train_ds = datasets.ImageFolder(os.path.join(input_path, "train"), data_transforms)
     val_ds = datasets.ImageFolder(os.path.join(input_path, "val"), data_transforms)
 
     dataloaders = {
-        "train": torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=4),
-        "validation": torch.utils.data.DataLoader(
-            val_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=4
-        ),
+        "train": torch.utils.data.DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=4),
+        "validation": torch.utils.data.DataLoader(val_ds, batch_size=batch_size, shuffle=True, num_workers=4),
     }
 
-    # Select device
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Using device: {device}")
+    return dataloaders
 
-    # Initialize ResNet50 model
+def initialize_model(device):
     model = models.resnet50(weights=ResNet50_Weights.DEFAULT).to(device)
 
-    # Data augmentation for training
-    train_ds = datasets.ImageFolder(
-        os.path.join(input_path, "train"),
-        transforms.Compose(
-            [
-                transforms.RandomResizedCrop(512),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            ]
-        ),
-    )
-
-    # Replace the last fully-connected layer with better weight initialization
     for layer in model.fc.modules():
         if isinstance(layer, nn.Linear):
             nn.init.kaiming_normal_(layer.weight)
             nn.init.zeros_(layer.bias)
 
-    # Create a new Sequential layer with the correct input and output size
     model.fc = nn.Sequential(
         nn.Linear(2048, 128), nn.ReLU(inplace=True), nn.Linear(128, 10), nn.Softmax(dim=1)
     ).to(device)
 
-    # Define loss function and optimizer
+    return model
+
+def save_epoch_weights(model, weights_dir, epoch):
+    epoch_weights_path = os.path.join(weights_dir, f"epoch_{epoch}.pth")
+    torch.save(model.state_dict(), epoch_weights_path)
+
+def train_one_epoch(model, criterion, optimizer, dataloader, device, phase):
+    model.train() if phase == "train" else model.eval()
+
+    count = 0
+    running_loss = 0.0
+    running_corrects = 0
+    all_labels = []
+    all_preds = []
+
+    with tqdm.tqdm(dataloader, desc=f"{phase} Epoch") as pbar:
+        for inputs, labels in pbar:
+            inputs = inputs.to(device)
+            labels = labels.to(device).long()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            if phase == "train":
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            preds = torch.argmax(outputs, dim=1)
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+            count += 1
+
+            # Collect labels and predictions for evaluation metrics
+            all_labels.extend(labels.cpu().numpy())
+            all_preds.extend(preds.cpu().numpy())
+
+    epoch_loss = running_loss / (count * dataloader.batch_size)
+    epoch_acc = running_corrects.float() / (count * dataloader.batch_size)
+
+    return epoch_loss, epoch_acc, all_labels, all_preds
+
+def evaluate_model(model, dataloader, device):
+    model.eval()
+
+    count = 0
+    all_labels = []
+    all_preds = []
+
+    with tqdm.tqdm(dataloader, desc="Evaluation") as pbar:
+        for inputs, labels in pbar:
+            inputs = inputs.to(device)
+            labels = labels.to(device).long()
+            outputs = model(inputs)
+
+            preds = torch.argmax(outputs, dim=1)
+            count += 1
+
+            # Collect labels and predictions for evaluation metrics
+            all_labels.extend(labels.cpu().numpy())
+            all_preds.extend(preds.cpu().numpy())
+
+    return all_labels, all_preds
+
+def train_model(model, criterion, optimizer, scheduler, dataloaders, device, num_epochs, loss_file_path, weights_dir):
+    best_val_acc = 0.0
+    best_model_wts = None
+
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch + 1}/{num_epochs}")
+
+        # Training Phase
+        train_loss, train_acc, _, _ = train_one_epoch(model, criterion, optimizer, dataloaders["train"], device, "train")
+
+        # Validation Phase
+        val_loss, val_acc, _, _ = train_one_epoch(model, criterion, None, dataloaders["validation"], device, "validation")
+
+        # Save the model weights after each epoch
+        save_epoch_weights(model, weights_dir, epoch + 1)
+
+        # Save the best model based on validation accuracy
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            best_model_wts = model.state_dict()
+
+        # Print classification report and confusion matrix
+        all_labels, all_preds = evaluate_model(model, dataloaders["validation"], device)
+        class_report = classification_report(all_labels, all_preds, zero_division=1)
+        print(f"Classification Report:\n{class_report}")
+
+        conf_matrix = confusion_matrix(all_labels, all_preds)
+        print(f"Confusion Matrix:\n{conf_matrix}")
+
+        print(f"Train loss: {train_loss:.4f}, acc: {train_acc:.4f}")
+        print(f"Validation loss: {val_loss:.4f}, acc: {val_acc:.4f}")
+        print(f"Current learning rate: {optimizer.param_groups[0]['lr']}")
+
+        # Save losses and accuracies to the file
+        with open(loss_file_path, "a") as loss_file:
+            loss_file.write(f"{epoch},{train_loss:.6f},{train_acc:.6f},{val_loss:.6f},{val_acc:.6f}\n")
+
+        scheduler.step(val_acc)  # Move this line outside the validation phase
+
+    return best_model_wts
+
+def main():
+    input_path = "./trainingDataset/Hair Diseases - Final"
+    BATCH_SIZE = 7
+    num_epochs = 40
+
+    dataloaders = load_data(input_path, BATCH_SIZE)
+    
+    # Select device
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")
+
+    model = initialize_model(device)
+
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)  # Adjust the learning rate
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode="max", patience=5, factor=0.1, verbose=True)
 
-    # Learning rate scheduler
-    scheduler = lr_scheduler.ReduceLROnPlateau(
-        optimizer, mode="max", patience=5, factor=0.1, verbose=True
-    )
-
-    # Create a file to save training and validation losses
     loss_file_path = "./losses/losses.csv"
-
-    with open(loss_file_path, "w") as loss_file:
-        loss_file.write("epoch,train_loss,train_acc,validation_loss,validation_acc\n")
-
-    # Define the directory to save epoch-wise weights
     weights_dir = "./trained_model_weights"
     os.makedirs(weights_dir, exist_ok=True)
 
-    def train_model_with_scheduler(
-        model, criterion, optimizer, scheduler, num_epochs, loss_file_path, weights_dir
-    ):
-        best_val_acc = 0.0
-        best_model_wts = None
-
-        for epoch in range(num_epochs):
-            print(f"Epoch {epoch + 1}/{num_epochs}")
-
-            train_loss, train_acc, val_loss, val_acc = 0, 0, 0, 0
-
-            for phase in ["train", "validation"]:
-                if phase == "train":
-                    model.train()
-                else:
-                    model.eval()
-
-                count = 0
-                running_loss = 0.0
-                running_corrects = 0
-                all_labels = []
-                all_preds = []
-
-                with tqdm.tqdm(dataloaders[phase]) as pbar:
-                    for inputs, labels in pbar:
-                        inputs = inputs.to(device)
-                        labels = labels.to(device).long()
-                        outputs = model(inputs)
-                        loss = criterion(outputs, labels)
-
-                        if phase == "train":
-                            optimizer.zero_grad()
-                            loss.backward()
-                            optimizer.step()
-
-                        preds = torch.argmax(outputs, dim=1)
-                        running_loss += loss.item() * inputs.size(0)
-                        running_corrects += torch.sum(preds == labels.data)
-                        count += 1
-
-                        # Collect labels and predictions for evaluation metrics
-                        all_labels.extend(labels.cpu().numpy())
-                        all_preds.extend(preds.cpu().numpy())
-
-                epoch_loss = running_loss / (count * BATCH_SIZE)
-                epoch_acc = running_corrects.float() / (count * BATCH_SIZE)
-
-                if phase == "train":
-                    train_loss = epoch_loss
-                    train_acc = epoch_acc
-                else:
-                    val_loss = epoch_loss
-                    val_acc = epoch_acc
-
-                    # Save the model weights after each epoch
-                    epoch_weights_path = os.path.join(weights_dir, f"epoch_{epoch + 1}.pth")
-                    torch.save(model.state_dict(), epoch_weights_path)
-
-                    # Save the best model based on validation accuracy
-                    if val_acc > best_val_acc:
-                        best_val_acc = val_acc
-                        best_model_wts = model.state_dict()
-
-                    # Print classification report and confusion matrix
-                    class_report = classification_report(all_labels, all_preds, zero_division=1)
-                    print(f"Classification Report:\n{class_report}")
-
-                    conf_matrix = confusion_matrix(all_labels, all_preds)
-                    print(f"Confusion Matrix:\n{conf_matrix}")
-
-                print(f"{phase} loss: {epoch_loss:.4f}, acc: {epoch_acc:.4f}")
-                print(f"Current learning rate: {optimizer.param_groups[0]['lr']}")
-
-                # Save losses and accuracies to the file
-                with open(loss_file_path, "a") as loss_file:
-                    loss_file.write(
-                        f"{epoch},{train_loss:.6f},{train_acc:.6f},{val_loss:.6f},{val_acc:.6f}\n"
-                    )
-
-            scheduler.step(val_acc)  # Move this line outside the validation phase
-
-        return best_model_wts
-
-    train_model_with_scheduler(
-        model, criterion, optimizer, scheduler, num_epochs=40, loss_file_path=loss_file_path, weights_dir=weights_dir
-    )
+    train_model(model, criterion, optimizer, scheduler, dataloaders, device, num_epochs, loss_file_path, weights_dir)
 
 if __name__ == '__main__':
     main()
--- nnTest.py
+++ nnTest.py
@@ -4,49 +4,71 @@ from torchvision import transforms
 from PIL import Image
 import torch.nn as nn
 
-# Load the ResNet50 model
-model = models.resnet50()
-model.fc = nn.Sequential(
-    nn.Linear(2048, 128), nn.ReLU(inplace=True), nn.Linear(128, 10), nn.Softmax(dim=1)
-)
+class HairDiseaseClassifier:
+    def __init__(self, model_path, class_labels, temperature=1.0):
+        self.class_labels = class_labels
+        self.temperature = temperature
+        self.model = self._load_model(model_path, class_labels)
 
-# Load the saved model weights
-model_path = "./trained_model_weights.pth"
-model.load_state_dict(torch.load(model_path))
+    def _load_model(self, model_path, class_labels):
+        model = models.resnet50()
+        model.fc = nn.Sequential(
+            nn.Linear(2048, 128), nn.ReLU(inplace=True), nn.Linear(128, len(class_labels)), nn.Softmax(dim=1)
+        )
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+        return model
 
-# Set the model to evaluation mode
-model.eval()
+    def preprocess_image(self, image_path):
+        transform = transforms.Compose(
+            [
+                transforms.Resize((256, 256)),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        image = Image.open(image_path)
+        image = transform(image)
+        image = image.unsqueeze(0)  # Add batch dimension
+        return image
 
+    def classify_image(self, image):
+        with torch.no_grad():
+            # Pass the input through temperature scaling
+            scaled_output = self._temperature_scale(self.model(image))
 
-def preprocess_image(image_path):
-    transform = transforms.Compose(
-        [
-            transforms.Resize((256, 256)),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
+        probabilities = torch.nn.functional.softmax(scaled_output[0], dim=0)
+        confidence, predicted_class = torch.max(probabilities, 0)
 
-    image = Image.open(image_path)
-    image = transform(image)
-    image = image.unsqueeze(0)  # Add batch dimension
-    return image
+        return confidence.item(), predicted_class.item()
 
+    def get_class_label(self, class_index):
+        return self.class_labels[class_index]
 
-def classify_image(model, image):
-    with torch.no_grad():
-        output = model(image)
+    def _temperature_scale(self, logits):
+        scaled_logits = logits / self.temperature
+        return scaled_logits
 
-    probabilities = torch.nn.functional.softmax(output[0], dim=0)
-    confidence, predicted_class = torch.max(probabilities, 0)
+def main():
+    model_path = "./trained_model_weights/epoch_17.pth"
+    class_labels = [
+        "Alopecia Areata", "Psoriasis", "Folliculitis", "Seborrheic Dermatitis",
+        "Lichen Planus", "Male Pattern Baldness", "Contact Dermatitis", "Head Lice",
+        "Telogen Effluvium", "Tinea Capitis"
+    ]
 
-    return confidence.item(), predicted_class.item()
+    temperature = 0.1  
 
+    classifier = HairDiseaseClassifier(model_path, class_labels, temperature)
 
-image_path_to_test = "C:/Users/roach/Desktop/archive (1)/Hair Diseases - Final/test/Folliculitis/folliculitis_0051.jpg"
-input_image = preprocess_image(image_path_to_test)
+    image_path_to_test = "./trainingDataset/Hair Diseases - Final/test/Head Lice/head_lice_0051.jpg"
+    input_image = classifier.preprocess_image(image_path_to_test)
 
-confidence, predicted_class = classify_image(model, input_image)
+    confidence, predicted_class = classifier.classify_image(input_image)
+    predicted_label = classifier.get_class_label(predicted_class)
 
-print(f"Predicted class: {predicted_class}, Confidence: {confidence * 100:.2f}%")
+    print(f"Predicted class: {predicted_label}, Confidence: {confidence * 100:.2f}%")
+
+if __name__ == "__main__":
+    main()
